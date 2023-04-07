68e9bbd09c148f1f67ec0235a97b23cd95b76575
Robert Gracey <robert.c.gracey@gmail.com>
2023-04-07 12:55:44 +1000

    fix: make whitespace checking more rigorous

--- pkg/tokeniser/misc.go
+++ pkg/tokeniser/misc.go
@@ -8,8 +8,23 @@ func isLetter(ch rune) bool {
 	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
 }
 
+// isWhitespace returns true if the given character is a whitespace character as
+// per the PDF spec.
+//
+// Whitespace characters:
+//   - 0x00 (null)
+//   - 0x09 (horizontal tab) \t
+//   - 0x0A (line feed) \n
+//   - 0x0C (form feed) \f
+//   - 0x0D (carriage return) \r
+//   - 0x20 (space)
 func isWhitespace(ch rune) bool {
-	return ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
+	switch ch {
+	case '\x00', '\t', '\n', '\f', '\r', ' ':
+		return true
+	}
+
+	return false
 }
 
 func isEol(ch rune) bool {
